da85b3c1f0c64707119c906ba52482a729e7a36c
Akshat Dubey <akshatdubey0808@gmail.com>
2024-04-07 02:06:09 +0530

    Protected: Frontend routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,29 +1,47 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Navbar } from "./components/navigation/Navbar";
 import Routes from "./components/navigation/Routes";
 import { Toaster } from "./components/ui/toaster";
 import { getUserDetails } from "./actions/userActions";
-import { useClerk } from "@clerk/clerk-react";
+import { useAuth, useClerk } from "@clerk/clerk-react";
 import { userAtom } from "./lib/store/store";
-import { useSetAtom } from "jotai";
+import { useAtom, useSetAtom } from "jotai";
+import Loading from "./components/Loading/Loading";
+import { useNavigate } from "react-router-dom";
 
 function App() {
   const { user } = useClerk();
+  const { isLoaded } = useAuth();
+  const [loading, setLoading] = useState(true);
   const setUser = useSetAtom(userAtom);
+  const navigate = useNavigate();
+
   useEffect(() => {
-    if (user) {
-      getUserDetails(
-        user.fullName,
-        user.primaryEmailAddress.emailAddress,
-        setUser
-      );
-    }
-  }, [user]);
+    const getDetails = async () => {
+      if (user) {
+        const response = await getUserDetails(
+          user.fullName,
+          user.primaryEmailAddress.emailAddress,
+          setUser
+        );
+        if (response.success) {
+          setLoading(false);
+        }
+      } else {
+        if (window.location.origin != "/") {
+          setLoading(false);
+          navigate("/");
+        }
+      }
+    };
+    getDetails();
+  }, [user, isLoaded]);
 
   return (
     <div className="min-h-screen w-full h-full font-Inter">
       <Toaster />
       <Navbar />
+      {loading && <Loading />}
       <Routes />
     </div>
   );
