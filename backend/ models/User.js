d43780e70f8f893ecab95a106c999e481981dcd1
om-chauhan1 <omchauhan64408@gmail.com>
2024-04-06 14:11:40 +0530

    Created User, Movie and Friendship models in backend

---
+++ backend/ models/User.js
@@ -0,0 +1,74 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    fullName: {
+      type: String,
+      required: true,
+    },
+    bio: {
+      type: String,
+      default: '',
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    age: {
+      type: Number,
+      default: null,
+    },
+    dateOfBirth: {
+      type: Date,
+      default: null,
+    },
+    role: {
+      type: String,
+      default: "user",
+      enum: ["user", "admin"],
+    },
+    gender: {
+      type: String,
+      enum: ["male", "female", "other"],
+      default: null,
+    },
+    location: {
+      country: {
+        type: String,
+      },
+      state: {
+        type: String,
+      },
+      city: {
+        type: String,
+      },
+    },
+    favoriteGenres: [
+      {
+        type: String,
+      },
+    ],
+    languagePreferences: [
+      {
+        type: String,
+      },
+    ],
+    friends: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Friendship",
+      },
+    ],
+    files: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "File",
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("User", userSchema);
