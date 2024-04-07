7670d7dbd179a19b2aa33112fc6c15b306433920
Akshat Dubey <akshatdubey0808@gmail.com>
2024-04-07 08:45:52 +0530

    To Remove: Profile.jsx

--- frontend/src/components/profile/Profile.jsx
+++ frontend/src/components/profile/Profile.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import { userAtom } from "@/lib/store/store";
+import { useAtom } from "jotai";
+import React from "react";
 
 const Profile = () => {
+  const user = useAtom(userAtom);
   return (
-    <div className='pt-20'>Profile</div>
-  )
-}
+    <div className="pt-16">
+      <div className="relative">
+        <img
+          src="https://static.vecteezy.com/system/resources/previews/000/677/302/original/abstract-technology-banner-background.jpg"
+          className="w-full h-full max-h-[20rem] overflow-hidden bg-zinc-300 object-cover aspect-video"
+        />
+        <img
+          src="https://is1-ssl.mzstatic.com/image/thumb/AMCArtistImages116/v4/74/9b/d1/749bd157-c81b-2c54-9940-16c13cecaa95/e5e09754-1d06-4a09-b2e1-6c24f77e0157_file_cropped.png/486x486bb.png"
+          alt="profile"
+          className="w-56 h-56 rounded-full absolute bottom-0 left-16 translate-y-1/3 border-2"
+        />
+      </div>
+      <div>
+        <h1 className="text-3xl font-bold pt-28 pl-16">Akshat Dubey</h1>
+        <p>{user?.bio}</p>
+      </div>
+    </div>
+  );
+};
 
-export default Profile
+export default Profile;
