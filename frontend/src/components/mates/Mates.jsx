7f927f51e1fd251f3d1d8325a1dfe53b03c5b486
Akshat Dubey <akshatdubey0808@gmail.com>
2024-04-07 06:56:18 +0530

    Fixed index.css

--- frontend/src/components/mates/Mates.jsx
+++ frontend/src/components/mates/Mates.jsx
@@ -22,66 +22,76 @@ const Mates = () => {
         <div className="col-span-1">
           <h1 className="text-3xl font-semibold">Incoming Requests</h1>
           <div className="pr-20 mt-5">
-            {incomingRequests.map((friend) => (
-              <div
-                key={friend.friend._id}
-                className="p-2 flex items-center justify-between gap-4 border rounded-xl px-4"
-              >
-                <div className="flex gap-4">
-                  <img
-                    src="https://is1-ssl.mzstatic.com/image/thumb/AMCArtistImages116/v4/74/9b/d1/749bd157-c81b-2c54-9940-16c13cecaa95/e5e09754-1d06-4a09-b2e1-6c24f77e0157_file_cropped.png/486x486bb.png"
-                    className="w-14 h-14 rounded-full"
-                  />
-                  <div>
-                    <h1 className="font-semibold">{friend.friend.fullName}</h1>
-                    <div className="flex items-center gap-1 font-medium text-secondary-foreground/50">
-                      {/* <h4>{friend.friend.age}</h4>
-                    <h4>{friend.friend.gender}</h4> */}
-                      <h4>Hello</h4>
-                      <h4>World</h4>
+            {incomingRequests.length > 0 ? (
+              incomingRequests.map((friend) => (
+                <div
+                  key={friend.friend._id}
+                  className="p-2 flex items-center justify-between gap-4 border rounded-xl px-4"
+                >
+                  <div className="flex gap-4">
+                    <img
+                      src="https://is1-ssl.mzstatic.com/image/thumb/AMCArtistImages116/v4/74/9b/d1/749bd157-c81b-2c54-9940-16c13cecaa95/e5e09754-1d06-4a09-b2e1-6c24f77e0157_file_cropped.png/486x486bb.png"
+                      className="w-14 h-14 rounded-full"
+                    />
+                    <div>
+                      <h1 className="font-semibold">
+                        {friend.friend.fullName}
+                      </h1>
+                      <div className="flex items-center gap-1 font-medium text-secondary-foreground/50">
+                        <h4>Hello</h4>
+                        <h4>World</h4>
+                      </div>
                     </div>
                   </div>
+                  <div className="space-x-3">
+                    <Button variant="ghost" className="hover:text-red-500">
+                      Decline
+                    </Button>
+                    <Button variant="ghost" className="hover:text-green-500">
+                      Accept
+                    </Button>
+                  </div>
                 </div>
-                <div className="space-x-3">
-                  <Button variant="ghost" className="hover:text-red-500">
-                    Decline
-                  </Button>
-                  <Button variant="ghost" className="hover:text-green-500">
-                    Accept
-                  </Button>
-                </div>
-              </div>
-            ))}
+              ))
+            ) : (
+              <p className="font-medium">There are no incoming requests</p>
+            )}
           </div>
         </div>
         <div>
           <h1 className="text-3xl font-semibold">Outgoing Requests</h1>
           <div className="pr-20 mt-5">
-            {incomingRequests.map((friend) => (
-              <div
-                key={friend.friend._id}
-                className="p-2 flex items-center justify-between gap-4 border rounded-xl px-4"
-              >
-                <div className="flex gap-3">
-                  <img
-                    src="https://is1-ssl.mzstatic.com/image/thumb/AMCArtistImages116/v4/74/9b/d1/749bd157-c81b-2c54-9940-16c13cecaa95/e5e09754-1d06-4a09-b2e1-6c24f77e0157_file_cropped.png/486x486bb.png"
-                    className="w-14 h-14 rounded-full"
-                  />
-                  <div>
-                    <h1 className="font-semibold">{friend.friend.fullName}</h1>
-                    <div className="flex gap-1 font-medium text-secondary-foreground/50">
-                      {/* <h4>{friend.friend.age}</h4>
+            {outgoingRequests.length > 0 ? (
+              outgoingRequests.map((friend) => (
+                <div
+                  key={friend.friend._id}
+                  className="p-2 flex items-center justify-between gap-4 border rounded-xl px-4"
+                >
+                  <div className="flex gap-3">
+                    <img
+                      src="https://is1-ssl.mzstatic.com/image/thumb/AMCArtistImages116/v4/74/9b/d1/749bd157-c81b-2c54-9940-16c13cecaa95/e5e09754-1d06-4a09-b2e1-6c24f77e0157_file_cropped.png/486x486bb.png"
+                      className="w-14 h-14 rounded-full"
+                    />
+                    <div>
+                      <h1 className="font-semibold">
+                        {friend.friend.fullName}
+                      </h1>
+                      <div className="flex gap-1 font-medium text-secondary-foreground/50">
+                        {/* <h4>{friend.friend.age}</h4>
                <h4>{friend.friend.gender}</h4> */}
-                      <h4>Hello, </h4>
-                      <h4>World</h4>
+                        <h4>Hello, </h4>
+                        <h4>World</h4>
+                      </div>
                     </div>
                   </div>
+                  <Button variant="ghost" className="hover:text-red-500">
+                    Cancel
+                  </Button>
                 </div>
-                <Button variant="ghost" className="hover:text-red-500">
-                  Cancel
-                </Button>
-              </div>
-            ))}
+              ))
+            ) : (
+              <p className="font-medium">There are no outgoing requests</p>
+            )}
           </div>
         </div>
       </div>
